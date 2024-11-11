1849614963aee085ccc392b2d67fa4fa15dcecc4
DelGrau <alanhipermassa@gmail.com>
2024-11-10 22:37:02 -0300

    Alteração do nome do arquivo

--- Pov_Front/lib/mobile/login_mobile.dart
+++ Pov_Front/lib/mobile/login_mobile.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
+import 'package:pov_web/Repository/UserRepository.dart';
 import 'package:pov_web/mobile/widgets/loginBoxMobile.dart';
 import 'package:pov_web/mobile/widgets/mobileAppBar.dart';
 
 class LoginMobile extends StatefulWidget {
-  const LoginMobile({super.key});
+  const LoginMobile({
+    super.key,
+  });
 
   @override
   State<LoginMobile> createState() => _LoginMobileState();
 }
 
 class _LoginMobileState extends State<LoginMobile> {
-  final userControler = TextEditingController();
+  final emailControler = TextEditingController();
   final senhaControler = TextEditingController();
+  final repository = UserRepository();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MobileAppBar(),
-        body: loginBoxMobile(userControler: userControler));
+        body: loginBoxMobile(
+          emailController: emailControler,
+          senhaController: senhaControler,
+          repository: repository,
+        ));
   }
 }
