b5bf9aa96c4ca705299d34d7bf094c3da115daed
Alan Henrique <alanhipermassa@gmail.com>
2024-11-20 16:09:48 -0300

    Primeira variação de itens na tela de vendas

--- Pov_Front/lib/Repository/ProdutoRepository.dart
+++ Pov_Front/lib/Repository/ProdutoRepository.dart
@@ -3,13 +3,22 @@ import 'package:pov_web/DataModels/Produto.dart';
 
 class ProdutoRepository {
   final Dio dio = Dio();
-  Produto? produto;
+  List<Produto> listProdutos = [];
 
-  Future<Produto> getAllProdutos() async {
-    var url = "http://192.168.1.9:8080/api/produto/todas";
-    Response response = await dio.post(url);
+  Future<List<Produto>> getAllProdutos() async {
+    var url = "http://192.168.100.113:8080/api/produto/todas";
+    Response response = await dio.get(url);
 
-    if (response.statusCode == 200) {}
-    return produto;
+    if (response.statusCode == 200) {
+      try {
+        for (var produtos in response.data) {
+          Produto produto = Produto.fromJson(produtos);
+          listProdutos.add(produto);
+        }
+      } catch (e) {
+        throw Exception(e.toString());
+      }
+    }
+    return listProdutos;
   }
 }
