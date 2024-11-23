77b010ccddbbf9f65924af85290ec80b3205cc15
Alan Henrique <alanhipermassa@gmail.com>
2024-11-23 14:03:56 -0300

    Adicionados os novos widgets

--- Pov_Front/lib/mobile/venda_mobile.dart
+++ Pov_Front/lib/mobile/venda_mobile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pov_web/mobile/widgets/button_mobile.dart';
 import 'package:pov_web/mobile/widgets/mobileAppBar.dart';
+import 'package:pov_web/mobile/widgets/venda_header.dart';
+import 'package:pov_web/mobile/widgets/venda_itens.dart';
+import 'package:pov_web/mobile/widgets/widget_cliente.dart';
 import 'package:pov_web/mobile/widgets/widget_itens_venda.dart';
 
 class VendaMobile extends StatefulWidget {
@@ -31,15 +34,42 @@ class _VendaMobileState extends State<VendaMobile> {
 
   @override
   Widget build(BuildContext context) {
+    final altura = MediaQuery.of(context).size.height;
+    final largura = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: MobileAppBar(pageTitle: "Venda"),
       body: Container(
+        //color: Colors.blue,
+        height: altura,
+        width: largura,
         alignment: Alignment.center,
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            children: itensVenda,
-          ),
+        child: Column(
+          children: [
+            VendaHeader(
+              // Onde aparecem os dados da venda, Cliente, Forma de Pagamento e Tabela de preço
+              altura: altura,
+              largura: largura,
+            ),
+            VendaItens(
+              // Onde aparecem os itens da Venda
+              altura: altura,
+              largura: largura,
+            ),
+            Container(
+              // Botão de Fechar pedido
+              margin: EdgeInsets.only(
+                bottom: altura * .025,
+                left: altura * .025,
+                right: altura * .025,
+              ),
+              child: mobileButton(
+                  buttonName: "Fechar Pedido",
+                  buttonFunction: fecharPedido,
+                  buttonWidth: largura,
+                  buttonHeight: altura * .07),
+            )
+          ],
         ),
       ),
     );
