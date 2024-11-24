3e2b050694972040a9ec6901ec06465f7d56e321
DelGrau <alanhipermassa@gmail.com>
2024-11-24 00:11:23 -0300

    Alterações na tela de checkout

--- Pov_Front/lib/mobile/checkout_mobile.dart
+++ Pov_Front/lib/mobile/checkout_mobile.dart
@@ -5,15 +5,24 @@ import 'package:pov_web/mobile/widgets/mobileAppBar.dart';
 class CheckoutMobile extends StatelessWidget {
   const CheckoutMobile({
     super.key,
-    required this.pedido,
+    this.pedido,
   });
 
-  final Pedido pedido;
+  final Pedido? pedido;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MobileAppBar(pageTitle: "Pedido: ${pedido.idPedido}"),
-    );
+    if (pedido != null) {
+      return Scaffold(
+        appBar: MobileAppBar(pageTitle: "Pedido: ${pedido!.idPedido}"),
+      );
+    } else {
+      return Scaffold(
+        appBar: MobileAppBar(pageTitle: "Pedido: NULL"),
+        body: const Center(
+          child: Text("Não foi possível localizar o pedido..."),
+        ),
+      );
+    }
   }
 }
