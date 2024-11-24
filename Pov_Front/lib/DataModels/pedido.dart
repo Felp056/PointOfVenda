cd72bdfdb5a3ce4d6e261301c82d9bc9627a4e6e
DelGrau <alanhipermassa@gmail.com>
2024-11-24 00:06:21 -0300

    Criada classe modelo de Pedido

--- Pov_Front/lib/DataModels/pedido.dart
+++ Pov_Front/lib/DataModels/pedido.dart
@@ -1,15 +1,31 @@
 import 'dart:ffi';
 
-class Pedido {
-  final Long idPedido;
-  final double valorPedido;
-  final String formaPagamento;
-  final double creditoDisponivel;
+import 'package:flutter/foundation.dart';
+import 'package:pov_web/DataModels/Produto.dart';
 
-  Pedido(
+class Pedido extends ChangeNotifier {
+  Long? idPedido;
+  double? valorPedido;
+  String? formaPagamento;
+  double? creditoDisponivel;
+
+  List<Produto>? produtos;
+
+  @override
+  Pedido();
+
+  @override
+  Pedido.iniciar(
     this.idPedido,
     this.valorPedido,
     this.formaPagamento,
     this.creditoDisponivel,
+    this.produtos,
   );
+
+  addProduto(item) {
+    produtos!.add(item);
+  }
+
+  salvar() {}
 }
